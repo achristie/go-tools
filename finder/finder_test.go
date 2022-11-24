c6e492f548b4110437c0263ebcbb90d75c4c2fe4
Andrew Christie <awchristie86@gmail.com>
2022-11-24 18:43:39 -0500

    ch 6, 7

---
+++ finder/finder_test.go
@@ -0,0 +1,63 @@
+package finder_test
+
+import (
+	"count/finder"
+	"os"
+	"testing"
+	"testing/fstest"
+)
+
+func TestFilesMemory(t *testing.T) {
+	t.Parallel()
+	fsys := fstest.MapFS{
+		"abc.go":          {},
+		"xyz.go":          {},
+		"sub/sub1.go":     {},
+		"sub/sub1.txt":    {},
+		"sub/sub/sub2.go": {},
+	}
+
+	want := 4
+	got := finder.Files(fsys)
+
+	if want != got {
+		t.Errorf("want %d, got %d", want, got)
+	}
+}
+
+func TestFilesDisk(t *testing.T) {
+	t.Parallel()
+	fsys := os.DirFS("testdata/")
+	want := 4
+	got := finder.Files(fsys)
+
+	if want != got {
+		t.Errorf("want %d, got %d", want, got)
+	}
+}
+
+func BenchmarkFilesOnDisk(b *testing.B) {
+	fsys := os.DirFS("testdata/")
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		finder.Files(fsys)
+	}
+}
+
+func BenchmarkFilesInMemory(b *testing.B) {
+	fsys := fstest.MapFS{
+		"abc.go":          {},
+		"xyz.go":          {},
+		"sub/sub1.go":     {},
+		"sub/sub1.txt":    {},
+		"sub/sub/sub2.go": {},
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+
+		finder.Files(fsys)
+	}
+
+}

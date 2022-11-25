98141fb028468366e88cfcb48e1fce55e0dbbf0e
Andrew Christie <awchristie86@gmail.com>
2022-11-25 18:22:00 -0500

    weather pt 1

---
+++ weather/weather.go
@@ -0,0 +1,125 @@
+package weather
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+const baseURL = "https://api.openweathermap.org"
+
+type Client struct {
+	key        string
+	BaseURL    string
+	HTTPClient *http.Client
+}
+
+type OWMResponse struct {
+	Weather []struct {
+		Main string
+	}
+}
+
+type Conditions struct {
+	Summary string `json:"summary"`
+}
+
+func NewClient(apikey string) *Client {
+	return &Client{
+		BaseURL: baseURL,
+		key:     apikey,
+		HTTPClient: &http.Client{
+			Timeout: 10 * time.Second,
+		},
+	}
+}
+
+func ParseResponse(data []byte) (Conditions, error) {
+	var owm OWMResponse
+	err := json.Unmarshal(data, &owm)
+	if err != nil {
+		return Conditions{}, err
+	}
+
+	if len(owm.Weather) < 1 {
+		return Conditions{}, fmt.Errorf("invalid response %s: no weather available", data)
+	}
+	return Conditions{Summary: owm.Weather[0].Main}, nil
+}
+
+func (c *Client) FormatURL(location string) string {
+	return fmt.Sprintf("%s/data/2.5/weather?q=%s&appid=%s", c.BaseURL, location, c.key)
+}
+
+func MakeAPIRequest(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		log.Fatal("unexpected status", resp.Status)
+	}
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return data, nil
+}
+
+func (c *Client) GetWeather(location string) (Conditions, error) {
+	URL := c.FormatURL(location)
+	resp, err := c.HTTPClient.Get(URL)
+	if err != nil {
+		return Conditions{}, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return Conditions{}, fmt.Errorf("unexpected response: %d", resp.StatusCode)
+	}
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return Conditions{}, err
+	}
+
+	conditions, err := ParseResponse(data)
+	if err != nil {
+		return Conditions{}, err
+	}
+
+	return conditions, nil
+}
+
+func Get(location, key string) (Conditions, error) {
+	c := NewClient(key)
+	conditions, err := c.GetWeather(location)
+	if err != nil {
+		return Conditions{}, err
+	}
+
+	return conditions, nil
+}
+
+func RunCLI() {
+
+	if len(os.Args) < 2 {
+		log.Fatalf("Usage: %s LOCATION\n\nExample: %[1]s London,UK", os.Args[0])
+	}
+	location := os.Args[1]
+	key := ""
+
+	conditions, err := Get(location, key)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(conditions)
+}
---
+++ weather/weather_test.go
@@ -0,0 +1,92 @@
+package weather_test
+
+import (
+	"count/weather"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestParseResponse(t *testing.T) {
+	t.Parallel()
+
+	data, err := os.ReadFile("testdata/weather.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := weather.Conditions{
+		Summary: "Clouds",
+	}
+	got, err := weather.ParseResponse(data)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
+	}
+}
+
+func TestParseResponseEmpty(t *testing.T) {
+	t.Parallel()
+	_, err := weather.ParseResponse([]byte{})
+	if err == nil {
+		t.Fatal("want error, got nil")
+	}
+}
+
+func TestResponseEmpty(t *testing.T) {
+	t.Parallel()
+	data, err := os.ReadFile("testdata/weather_empty.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = weather.ParseResponse(data)
+	if err == nil {
+		t.Fatal("want error, got nil")
+	}
+}
+
+func TestFormatURL(t *testing.T) {
+	t.Parallel()
+	c := weather.NewClient("dummy")
+	location := "Paris,FR"
+	want := "https://api.openweathermap.org/data/2.5/weather?q=Paris,FR&appid=dummy"
+	got := c.FormatURL(location)
+
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
+	}
+}
+
+func TestGetWeather(t *testing.T) {
+	t.Parallel()
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		f, err := os.Open("testdata/weather.json")
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer f.Close()
+		io.Copy(w, f)
+	}))
+	defer ts.Close()
+
+	c := weather.NewClient("dummy")
+	c.BaseURL = ts.URL
+	c.HTTPClient = ts.Client()
+
+	want := weather.Conditions{
+		Summary: "Clouds",
+	}
+	got, err := c.GetWeather("Paris,FR")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
+	}
+}
